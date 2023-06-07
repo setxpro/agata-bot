6dc350c0f9e88f2ba65191052bf566bb01f3116f
Patrick Anjos <69186374+setxpro@users.noreply.github.com>
2023-06-07 14:32:20 -0300

    add readme

--- src/functions/formatDate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function formatCurrentDate(date) {
-    const year = date.slice(0, 4);
-    const month = date.slice(4, 6);
-    const day = date.slice(6, 8);
-    return `${day}/${month}/${year}`;
-}
-exports.default = formatCurrentDate;
